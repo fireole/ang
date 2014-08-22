a2c01c5eef78f84cfc179c503fcaa4345d697283
Antonio Olander <antonio.olander@hotmail.com>
2014-08-21 22:47:54 -0700

    added the usercontroller

---
+++ Source/TempMask/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace TempMask.Controllers
+{
+    public class UserController : ApiController
+    {
+
+        [Route("Par")]
+        public dynamic Get()
+        {
+            var user = new User();
+            user.FirstName = "Antonio";
+
+            return JsonConvert.SerializeObject(user);
+        }
+
+    }
+
+    internal class User
+    {
+        public string FirstName { get; set; }
+
+    }
+}
